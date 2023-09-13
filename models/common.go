899d27d6a2006c89ec43c4596e6a5d402cdd3cd6
xishengcai <1dd.163.com>
2023-09-13 08:06:31 +0800

    update

---
+++ models/common.go
@@ -0,0 +1,77 @@
+package models
+
+import (
+	"gorm.io/gorm"
+
+	"issedb-resource/pkg/setting"
+)
+
+type IDs struct {
+	IDs []string `json:"ids"`
+}
+
+// RestPage 分页查询
+// page  设置起始页、每页条数,
+// name  查询目标表的名称
+// query 查询条件,
+// dest  查询结果绑定的结构体,
+// bind  绑定表结构对应的结构体
+func restPage(page Page, name string, query interface{}, dest interface{}, bind interface{}) (int64, error) {
+	if page.PageNum > 0 && page.PageSize > 0 {
+		offset := (page.PageNum - 1) * page.PageSize
+		setting.DB.Offset(offset).Limit(page.PageSize).Table(name).Where(query).Order("created desc").Find(dest)
+	}
+	res := setting.DB.Table(name).Where(query).Order("created desc").Find(bind)
+	return res.RowsAffected, res.Error
+}
+
+type Page struct {
+	PageNum  int `form:"pageNum,default=1"`
+	PageSize int `form:"pageSize,default=10"`
+}
+
+func (p Page) Offset() int {
+	if p.PageNum < 1 {
+		p.PageNum = 1
+	}
+	return (p.PageNum - 1) * p.PageSize
+
+}
+
+func (p Page) Limit() int {
+	if p.PageSize <= 0 {
+		return 10
+	}
+	return p.PageSize
+}
+
+func GetDB(tableName string) DBHelp {
+	return DBHelp{
+		Table: tableName,
+		DB:    setting.DB.Table(tableName),
+	}
+}
+
+type DBHelp struct {
+	Table string
+	*gorm.DB
+}
+
+func (db DBHelp) Find(id string, dest interface{}) (interface{}, error) {
+	err := db.Where("id = ?", id).First(&dest).Error
+	return dest, err
+}
+
+func (db DBHelp) Query(page Page, result interface{}) (res interface{}, total int64, err error) {
+	err = db.Count(&total).
+		Limit(page.PageSize).
+		Offset(page.Offset()).
+		Order("updated_time desc").
+		Find(&result).Error
+	res = result
+	return
+}
+
+func (db DBHelp) Del(ids []string) error {
+	return db.Where("id in ?", ids).Delete(db.Table).Error
+}
---
+++ pkg/db/gorm.go
@@ -0,0 +1,64 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+
+	"issedb-resource/models"
+	"issedb-resource/pkg/setting"
+)
+
+func InitMysql() {
+	m := setting.Config.Mysql
+	conn, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=%s&parseTime=%t&loc=Local",
+		m.Username,
+		m.Password,
+		m.Host,
+		m.DBName,
+		m.Charset,
+		m.ParseTime))
+	if err != nil {
+		panic(err)
+	}
+	conn.SetMaxIdleConns(m.MaxIdleConnections)
+	conn.SetMaxOpenConns(m.MaxOpenConnections)
+	setting.DB, err = gorm.Open(
+		mysql.New(mysql.Config{Conn: conn}),
+		&gorm.Config{
+			Logger:                 newLogger,
+			NamingStrategy:         schema.NamingStrategy{SingularTable: true},
+			SkipDefaultTransaction: true,
+		})
+
+	if err != nil {
+		panic(err)
+	}
+
+	setting.DB.Debug()
+	err = setting.DB.AutoMigrate(&models.Host{})
+	if err != nil {
+		fmt.Errorf("数据库表结构迁移失败，%s", err.Error())
+		panic(err)
+	}
+}
+
+var (
+	newLogger = logger.New(
+		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
+		logger.Config{
+			SlowThreshold:             time.Second, // Slow SQL threshold
+			LogLevel:                  logger.Info, // Log level
+			IgnoreRecordNotFoundError: true,        // Ignore ErrRecordNotFound error for logger
+			ParameterizedQueries:      true,        // Don't include params in the SQL log
+			Colorful:                  false,       // Disable color
+		},
+	)
+)
